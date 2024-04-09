565afc267b956c5c7d09407dc6d6b7d402e35907
rliu6915 <rliu6915@connect.hku.hk>
2024-04-09 15:37:11 +0800

    tokenExtractor modify

--- util/middleware.js
+++ util/middleware.js
@@ -1,11 +1,32 @@
 const jwt = require('jsonwebtoken')
 const { SECRET } = require('./config.js')
+const Session = require('../models/session.js')
+const User = require('../models/user.js')
+
+const sessionFind = async (token) => {
+  return await Session.findOne({
+    where: {
+      token
+    },
+    include: {
+      model: User
+    }
+  })
+}
 
 const tokenExtractor = (req, res, next) => {
   const authorization = req.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     try {
       req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+      const session = sessionFind(req.decodedToken)
+      if (!session) {
+        return res.status(401).json({ error: 'token invalid' })
+      }
+      if (session.user.disabled) {
+        return res.status(401).json({ error: 'user disabled' })
+      }
+      req.user = session.user
     } catch{
       return res.status(401).json({ error: 'token invalid' })
     }
@@ -16,7 +37,7 @@ const tokenExtractor = (req, res, next) => {
 }
 
 const validSession = (req, res, next) => {
-  
+
   next()
 }
 
